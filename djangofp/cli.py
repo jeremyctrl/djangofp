55ce15dc7dc88b1d9529ef900c8d0fdeda9bd38f
Jeremy <235932977+jeremyctrl@users.noreply.github.com>
2025-10-05 06:31:08 +0300

    chore(release): 0.1.6

--- djangofp/cli.py
+++ djangofp/cli.py
@@ -102,7 +102,7 @@ def main():
         default="/static/admin/css/",
         help="Static path (default: /static/admin/css/)",
     )
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.1.5")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.1.6")
     args = parser.parse_args()
 
     try:
